993a090d38b6ec139a935e428a027dcd37e41864
ouKD <111720334+ouKD@users.noreply.github.com>
2023-08-29 17:39:47 +1000

    70% week4lab

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
+    [SerializeField]
+    private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-    
+        Instantiate(redObj, new Vector3(2.0f, 0.0f, 0.0f), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2.0f, 0.0f, 0.0f), Quaternion.identity);
+        Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
